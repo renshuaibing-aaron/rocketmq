3452cad8b36ef04296310d75d2c6c38eaa8ed84e
renshuaibing <renshuaibing@hikvision.com.cn>
2020-04-28 18:58:29 +0800

    分析

--- example/src/main/java/org/apache/rocketmq/example/filter/TagFilterConsumer.java
+++ example/src/main/java/org/apache/rocketmq/example/filter/TagFilterConsumer.java
@@ -1,6 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.rocketmq.example.filter;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
@@ -8,24 +23,15 @@ import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.client.exception.MQClientException;
-import org.apache.rocketmq.common.MixAll;
 import org.apache.rocketmq.common.message.MessageExt;
 
-public class Consumer {
+public class TagFilterConsumer {
 
     public static void main(String[] args) throws InterruptedException, MQClientException, IOException {
-        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("ConsumerGroupNamecc4");
 
-        consumer.setNamesrvAddr("127.0.0.1:9876");
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-       // File classFile = new File(classLoader.getResource("MessageFilterImpl.java").getFile());
-        File classFile = new File("E:\\HR-code\\20190116\\活动报名\\rocketmq\\example\\src\\main\\resources\\MessageFilterImpl.java");
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("please_rename_unique_group_name");
 
-        System.out.println("------22---------"+classFile);
-        String filterCode = MixAll.file2String(classFile);
-        System.out.println("---------------"+filterCode);
-        consumer.subscribe("TopicFilterTest2", "org.apache.rocketmq.example.filter.MessageFilterImpl",
-            filterCode);
+        consumer.subscribe("TagFilterTest", "TagA || TagC");
 
         consumer.registerMessageListener(new MessageListenerConcurrently() {
 
@@ -39,6 +45,6 @@ public class Consumer {
 
         consumer.start();
 
-        System.out.printf("PushConsumer Started.%n");
+        System.out.printf("Consumer Started.%n");
     }
 }
